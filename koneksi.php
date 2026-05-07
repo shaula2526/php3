7eee6b7b69168c8d28f0c075d05f538773197ca4
shaulamuliawan <shaulamuliawan@gmail.com>
2026-05-07 14:54:37 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,22 @@
+<?php
+
+// Mengizinkan semua origin (untuk development)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// PHP akan mencoba mengambil data asli dari Railway dulu, 
+// kalau tidak ada baru pakai data di dalam kutip.
+$host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
+$user = getenv('MYSQLUSER') ?: "root";
+$pass = getenv('MYSQLPASSWORD') ?: "LZstNChgUtUIzpLIpzjVtJVusCMZscPX";
+$db   = getenv('MYSQLDATABASE') ?: "railway";
+$port = getenv('MYSQLPORT') ?: 3306;
+
+$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
+
+if (!$koneksi) {
+    // Menampilkan error yang lebih spesifik jika gagal
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
